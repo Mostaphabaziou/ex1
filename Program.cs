e325d9a5722919b37c4ae2f0e578736aa52ee6c7
Mostaphabaziou <92397218+Mostaphabaziou@users.noreply.github.com>
2021-10-13 04:25:39 +0100

    EX 1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        void prime_num(long num)
+        {
+            bool isPrime = true;
+            for (int i = 0; i <= num; i++)
+            {
+                for (int j = 2; j <= num; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("Prime:" + i);
+                }
+                isPrime = true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.prime_num(999999999999999L);
+            Console.ReadLine();
+        }
+    }
+}
